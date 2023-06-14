1df0009d1677bb74ef5d51aaffe8efdf97e82aa1
chaewon <cwlee4633@naver.com>
2023-06-14 11:43:29 +0900

    clear - 완성!

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -25,54 +25,50 @@ public class Main {
             n[i] = index;
         }
 
-        for(int value : n) {
-            System.out.println(value);
-        }
+//        for (int value : n) {
+//            System.out.println(value);
+//        }
 
         System.out.println("-------------------------------------------");
 
         //사용자는 컴퓨터가 뽑은 숫자를 맞추기 위해 시도합니다.
         Scanner sc = new Scanner(System.in);
         System.out.println("컴퓨터가 숫자를 생성하였습니다. 답을 맞춰보세요!");
-        String number = sc.nextLine();
-        if(number.length()==3){
-            int first_num = number.charAt(0) -'0';
-            int second_num = number.charAt(1) -'0';
-            int third_num = number.charAt(2) -'0';
-            int[] box = {first_num, second_num, third_num};
-            System.out.println(first_num);
-            System.out.println(second_num);
-            System.out.println(third_num);
-            System.out.println(Arrays.toString(box));
 
-            //컴퓨터는 사용자가 입력한 세자리 숫자에 대해서, 아래의 규칙대로 스트라이크(S)와 볼(B)를 알려줍니다.
-            //1. box의 요소들을 하나씩 돌고, n의 요소들을 하나씩 돌아서 두 요소의 숫자와 위치가 같을 때, s++
-            //2. box의 요소들과 n의 요소들 중에 같은 숫자가 있으면 그 수 만큼 b++
-            int b = 0;
-            int s = 0;
+        //기회는 무제한이며, 몇번의 시도 후에 맞췄는지 기록됩니다.
+        //무제한 = 반복문 for while break continue
+        int count = 0;
+        int b = 0;
+        int s = 0;
+        while (s != 3) {
+            System.out.print( ++count +"번째 시도 : ");
+            String number = sc.nextLine();
+            if (number.length() == 3) {
+                b = 0;
+                s = 0;
 
-            for (int i = 0; i < 3; i++) {
-                if (box[i] == n[i]){
-                    s++;
-                } else if(box[i] == n[0]) {
-                    b++;
-                } else if(box[i] == n[1]) {
-                    b++;
-                } else if(box[i] == n[2]) {
-                    b++;
+                int first_num = number.charAt(0) - '0';
+                int second_num = number.charAt(1) - '0';
+                int third_num = number.charAt(2) - '0';
+                int[] box = {first_num, second_num, third_num};
+
+                for (int i = 0; i < 3; i++) {
+                    if (box[i] == n[i]) {
+                        s++;
+                    } else if (box[i] == n[0]) {
+                        b++;
+                    } else if (box[i] == n[1]) {
+                        b++;
+                    } else if (box[i] == n[2]) {
+                        b++;
+                    }
                 }
+                System.out.println(b+"B"+s+"S");
+            } else {
+                System.out.println("숫자의 개수가 맞지 않습니다");
             }
-            System.out.println(s);
-            System.out.println(b);
-
-
         }
-
+        System.out.println( count + "번만에 맞히셨습니다.");
+        System.out.println( "게임을 종료합니다.");
     }
 }
-
-
-
-
-//기회는 무제한이며, 몇번의 시도 후에 맞췄는지 기록됩니다.
-//숫자 3개를 모두 맞춘 경우, 게임을 종료합니다.
